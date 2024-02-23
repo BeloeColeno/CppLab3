1b5a797323cad6aab0208985d82f0948328d563f
Семён Петров <semion.semen2013@gmail.com>
2024-02-23 15:38:45 +0500

    working version

--- C++Lab3/Bubble.cpp
+++ C++Lab3/Bubble.cpp
@@ -3,20 +3,21 @@ using namespace std;
 
 void bubble(int arr[], int size)
 {
-	for (int i = 0; i +1 < size; i++)
+	for (int i = 0; i + 1 < size; i++)
 	{
-		for (int j = 0; j +1 < size -i; j++)
+		for (int j = 0; j + 1 < size -i; j++)
 		{
 			if (arr[j+1] < arr[j])
 			{
-				int t = j;
-				arr[j] = arr[j + 1];
-				arr[j + 1] = j;
+				swap(arr[j], arr[j + 1]);
 			}
 		}
 	}
-	for (int i = 0; i < size; i++)
-	{
-		cout << arr[i] << ' ';
-	}
+}
+
+static void swap(int& left, int& right)
+{
+	int temp = left;
+	left = right;
+	right = temp;
 }
--- C++Lab3/MiniMax.cpp
+++ C++Lab3/MiniMax.cpp
@@ -6,20 +6,16 @@ void miniMax(int arr[], int size)
 	//текущие индексы мин и макс элементов
 	int imin, imax;
 	//вычисление n-колличества элементов
-	int n = size;
 	int i;
 	//установка начальных индексов для мин и макс;
 	imin = i = 0; imax = i = 0;
-	for (i = 0; i < n - 1; i++) //нахождение мин и макс
+	for (i = 0; i < size - 1; i++) //нахождение мин и макс
 	{
 		imin = i;
-		for (int j = i + 1; j < n; j++)
+		for (int j = i + 1; j < size; j++)
 			if (arr[j] < arr[imin]) imin = j;
 		int t = arr[i];
 		arr[i] = arr[imin];
 		arr[imin] = t;
 	}
-	for (i = 0; i < n; i++) //вывод на экран
-		cout << arr[i] << ' ';
-	cout << endl;
 }
